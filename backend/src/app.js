c4aecac0d6c892a24ccc9e67d19d53451670fadf
Prashant Kumar <prashantkumar6425@gmail.com>
2025-12-13 22:09:32 +0530

    feat: setip express app and server bootstrap

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,12 @@ const app = express();
 
 // import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js";
+import authRoutes from "./modules/auth/auth.routes.js";
+
+// middlewares
+app.use(express.json());
 app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Welcome to Sweet-Shop-Management-System.");
