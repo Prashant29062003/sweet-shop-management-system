75b650384da1896cc325f0d5c848dcb458678935
Prashant Kumar <prashantkumar6425@gmail.com>
2025-12-14 21:30:28 +0530

    refractor(app): extract layout and routing from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
-const App = () => {
+import AppLayout from "./layouts/AppLayout";
+import Login from "./pages/Login";
+import Sweets from "./pages/Sweets";
+import Inventory from "./pages/Inventory";
+import ProtectedRoute from "./components/common/ProtectedRoute";
+
+export default function App() {
   return (
-    <div className='bg-amber-800'>
-      <h1 className='text-8xl text-white '>Sweet Shop</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/login" element={<Login />} />
 
-export default App
+            <Route element={<ProtectedRoute />}>
+              <Route path="/sweets" element={<Sweets />} />
+              <Route path="/inventory" element={<Inventory />} />
+            </Route>
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
