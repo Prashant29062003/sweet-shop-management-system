3fba397a262c76d5bbf3f19789012954aee2dd6f
Prashant Kumar <prashantkumar6425@gmail.com>
2025-12-14 12:34:10 +0530

    feat(permissions): define permission constants

---
+++ backend/src/utils/constants/permissions.js
@@ -0,0 +1,20 @@
+export const PERMISSIONS = {
+    // Sweet Management
+    CREATE_SWEET: "create:sweet",
+    UPDATE_SWEET: "update:sweet",
+    DELETE_SWEET: "delete:sweet",
+
+    // Inventory Management
+    VIEW_INVENTORY: "view:inventory",
+    UPDATE_INVENTORY: "update:inventory",
+};
+export const AvailablePermissions = Object.values(PERMISSIONS);
+
+
+
+export const TaskStatusEnum = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done"
+};
+export const AvailableTaskStatuses = Object.values(TaskStatusEnum);
