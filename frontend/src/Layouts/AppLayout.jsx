8e5ee4310be378c714402b78343a687d89995e0b
Prashant Kumar <prashantkumar6425@gmail.com>
2025-12-15 16:08:31 +0530

    fix(deps): update dependency lodash to address security vulnerability

--- frontend/src/Layouts/AppLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function AppLayout() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Outlet />
-    </div>
-  );
-}
